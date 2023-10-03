036495bc70d3dcfb725d7f04390e249b4381df61
Perry Baran <perrybaran@gmail.com>
2023-10-03 11:23:31 +0100

    added fire modal button and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import ConfirmationModal from './ConfirmationModal';
+import { useState } from 'react';
+import ConfirmationModal from './components/ConfirmationModal/ConfirmationModal';
 
 function App() {
+  const [showModal, setShowModal] = useState([])
+
+  const onFireModalClick = () => {
+    setShowModal(prev => {
+      const clone = [...prev]
+      clone.push(true)
+      return clone
+    })
+  }
+
+  const onCancelClick = (index) => {
+    return () => setShowModal(prev => {
+      const clone = [...prev]
+      clone.splice(index, 1)
+      return clone;
+    })
+  }
+
   return (
     <div>
-      <ConfirmationModal />
+      <button onClick={onFireModalClick}>Fire Modal</button>
+      {showModal.map((_, index) => {
+        return (
+          <ConfirmationModal onCancelClick={onCancelClick(index)}/>
+        )
+      })}
     </div>
   );
 }
